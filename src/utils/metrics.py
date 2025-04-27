a651fbd1dae8a328048640691705d0e77d96e94b
vaibhav sourirajan <vs2787@columbia.edu>
2025-04-27 19:03:13 -0400

    fixed mean metric

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -4,4 +4,5 @@ def calculate_metrics(y_true, y_pred):
     mse = np.mean((y_true - y_pred) ** 2)
     rmse = np.sqrt(mse)
     mae = np.mean(np.abs(y_true - y_pred))
-    return {'MSE': mse, 'RMSE': rmse, 'MAE': mae}
+    mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    return {'MSE': mse, 'RMSE': rmse, 'MAE': mae, 'MAPE': mape}
